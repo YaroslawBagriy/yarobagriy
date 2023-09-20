61b3ccd01b18355d828d67df8ced3c7dcb0957cf
Yaroslaw Bagriy <yaroslawbagriy@gmail.com>
2023-09-20 17:09:51 -0500

    Finalized website and added Fathom script

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 font-sans min-h-screen md:justify-center max-w-2xl mx-auto w-full text-gray-900">
       <Header siteTitle={"Yaro Bagriy"} />
 
-      <main className="flex flex-col flex-1 md:justify-center max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
+      <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
         <div className="text-center">
           <h1 className="text-2xl font-bold text-indigo-600 leading-none font-inter space-x-48">
             I create digital products and invest in income producing assets.
@@ -22,7 +22,7 @@ export default function Home() {
           <br />
           <p className="mt-1 text-base text-gray-700 leading-relaxed text-left font-body">
             My goal is to build enough income producing assets to fully sustain my lifestyle. A few of those
-            assets include SaaS projects, podcasts, newsletters, crypto, communities, real
+            assets include SaaS projects, podcasts, newsletters, crypto, real
             estate rentals, and a stock dividend portfolio.
           </p>
           <br />
@@ -30,6 +30,7 @@ export default function Home() {
             My non-work hobbies include triathlons, collecting Pokemon cards, traveling, poker, chess, MMA, and spending
             quality time with my family.
           </p>
+          <br />
           <br />
           <h2 className="text-xl font-bold text-gray-800 leading-none text-left">
             Projects
@@ -109,52 +110,34 @@ export default function Home() {
                       <tr>
                         <th scope="col" class="px-6 py-4">Website</th>
                         <th scope="col" class="px-6 py-4">Description</th>
-                        <th scope="col" class="px-6 py-4">Medium</th>
-                        <th scope="col" class="px-6 py-4">Link</th>
+                        <th scope="col" class="px-6 py-4">Date</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr class="border-b dark:border-neutral-500 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">Community Finder</td>
-                        <td class="whitespace-normal px-6 py-4">Managing Indie stack community with a full time job with Yaro</td>
-                        <td class="whitespace-normal px-6 py-4">Podcast</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://www.community-finder.co/podcasts/managing-indie-stack-community">Listen here</a></td>
-                      </tr>
-                      <tr class="border-b bg-gray-100 dark:border-neutral-800 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">Learnkn</td>
-                        <td class="whitespace-normal px-6 py-4">Indie Lifestyle, Managing Time, and How to Build Community!</td>
-                        <td class="whitespace-normal px-6 py-4">Podcast</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://www.youtube.com/watch?v=6OezrHYCK5c">Listen here</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://podcasts.apple.com/us/podcast/18-yaroslaw-bagriy-interview/id1503184253?i=1000484640098">Ship Building</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://podcasts.apple.com/us/podcast/18-yaroslaw-bagriy-interview/id1503184253?i=1000484640098">Yaroslaw Bagriy Interview</a></td>
+                        <td class="whitespace-normal px-6 py-4">Jul 13, 2020</td>
                       </tr>
                       <tr class="border-b dark:border-neutral-500 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">Ship Building</td>
-                        <td class="whitespace-normal px-6 py-4">Yaroslaw Bagriy Interview</td>
-                        <td class="whitespace-normal px-6 py-4">Podcast</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://podcasts.apple.com/us/podcast/18-yaroslaw-bagriy-interview/id1503184253?i=1000484640098">Listen here</a></td>
-                      </tr>
-                      <tr class="border-b bg-gray-100 dark:border-neutral-800 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">IndieHackers</td>
-                        <td class="whitespace-normal px-6 py-4">Making a Living From the Paid Newsletter Ecosystem</td>
-                        <td class="whitespace-normal px-6 py-4">Podcast</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://www.indiehackers.com/podcast/191-yaroslaw-bagriy-of-newsletter-crew">Listen here</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.radletters.com/blog/newsletter-crew-a-podcast-for-newsletter-creators">Radletters</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.radletters.com/blog/newsletter-crew-a-podcast-for-newsletter-creators">Newsletter Crew: A Podcast for Newsletter Creators</a></td>
+                        <td class="whitespace-normal px-6 py-4">Jul 30, 2020</td>
                       </tr>
                       <tr class="border-b dark:border-neutral-500 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">IndieHackers</td>
-                        <td class="whitespace-normal px-6 py-4">Newsletter Crew: A Podcast for Newsletter Creators</td>
-                        <td class="whitespace-normal px-6 py-4">Blog</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://www.radletters.com/blog/newsletter-crew-a-podcast-for-newsletter-creators">Read here</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://alternativeassets.club/an-interview-with-yaro-bigry/">Alts</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://alternativeassets.club/an-interview-with-yaro-bigry/">The World of Newsletter Acquisitions: Interview with Yaro Bagriy</a></td>
+                        <td class="whitespace-normal px-6 py-4">September 4, 2020</td>
                       </tr>
                       <tr class="border-b bg-gray-100 dark:border-neutral-800 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">Community Finder</td>
-                        <td class="whitespace-normal px-6 py-4">How Newsletter Crew acquired IndieMailer in 1 day</td>
-                        <td class="whitespace-normal px-6 py-4">Blog</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://www.community-finder.co/post/acquiring-a-communitys">Read here</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.youtube.com/watch?v=6OezrHYCK5c">Learnkn</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.youtube.com/watch?v=6OezrHYCK5c">Indie Lifestyle, Managing Time, and How to Build Community!</a></td>
+                        <td class="whitespace-normal px-6 py-4">Oct 22, 2020</td>
                       </tr>
-                      <tr class="border-b dark:border-neutral-500 no-scrollbar hover:bg-indigo-500 hover:text-white">
-                        <td class="whitespace-normal px-6 py-4">Alts</td>
-                        <td class="whitespace-normal px-6 py-4">The World of Newsletter Acquisitions: Interview with Yaro Bagriy</td>
-                        <td class="whitespace-normal px-6 py-4">Blog</td>
-                        <td class="whitespace-normal px-6 py-4"><a href="https://alternativeassets.club/an-interview-with-yaro-bigry/">Read here</a></td>
+                      <tr class="border-b bg-gray-100 dark:border-neutral-800 no-scrollbar hover:bg-indigo-500 hover:text-white">
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.indiehackers.com/podcast/191-yaroslaw-bagriy-of-newsletter-crew">IndieHackers</a></td>
+                        <td class="whitespace-normal px-6 py-4"><a href="https://www.indiehackers.com/podcast/191-yaroslaw-bagriy-of-newsletter-crew">Making a Living From the Paid Newsletter Ecosystem</a></td>
+                        <td class="whitespace-normal px-6 py-4">February 15, 2021</td>
                       </tr>
                     </tbody>
                   </table>
